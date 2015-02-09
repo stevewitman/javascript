c0f556a93de18ba80f7ecdba8560b9f4c1255887
Steve Witman <stevewitman@gmail.com>
2015-02-08 22:26:13 -0700

    added fix_broken_code

---
+++ 04_fix_broken_code/spec/spec.js
@@ -0,0 +1,45 @@
+var yourCode = require('../src');
+
+describe('addition', function() {
+  it('returns the result of adding the two argument', function() {
+    expect(yourCode.addition(1,2)).toEqual(3);
+  });
+});
+
+describe('matches', function() {
+  it('returns true if the given arguments match', function() {
+    expect(yourCode.matches('a', 'a')).toEqual(true);
+  });
+
+  it('returns false if the given arguments do not match', function() {
+    expect(yourCode.matches('a', 'b')).toEqual(false);
+  });
+});
+
+describe('fizzBuzz', function() {
+  it('returns fizz when the argument is divisible by 3', function() {
+    expect(yourCode.fizzBuzz(3)).toEqual('Fizz');
+  });
+
+  it('returns buzz when the argument is divisible by 5', function() {
+    expect(yourCode.fizzBuzz(5)).toEqual('Buzz');
+  });
+
+  it('returns the number itself when it is not divisible by 3 or 5', function() {
+    expect(yourCode.fizzBuzz(7)).toEqual(7);
+  });
+});
+
+describe('switcher', function() {
+  it('returns "never output anything. ever!" for arguments that are not "foo" or "nope"', function() {
+    expect(yourCode.switcher('random')).toEqual('never output anything. ever!');
+  });
+
+  it('returns "bar" for argument "foo"', function() {
+    expect(yourCode.switcher('foo')).toEqual('bar');
+  });
+
+  it('returns "yup" for argument "nope"', function() {
+    expect(yourCode.switcher('nope')).toEqual('yup');
+  });
+});
---
+++ 04_fix_broken_code/src.js
@@ -0,0 +1,56 @@
+/*
+  In these warm-ups, all of the code is provided, as are the failing tests. Locate
+  the defect in each function and make all of the tests green. Note: when you change
+  some tests from pending -> active, they may pass initially (keep going!).
+*/
+
+module.exports = {
+  addition: function(a,b) {
+    var return a + b;
+  },
+
+  matches: function(first, second) {
+    var result;
+
+    if (first === second) {
+      var result = true;
+    } else {
+      result = false;
+    }
+
+    return result;
+  },
+
+  fizzBuzz: function(number) {
+    var divisibleByThree = number % 3,
+        divisibleByFive = number % 5,
+        output = number;
+
+    if (divisibleByThree === 0 && divisibleByFive === 0) {
+      output = 'FizzBuzz';
+    } else if (divisibleByThree === 0) {
+      output = 'Fizz';
+    } else if (divisibleByFive === 0) {
+      output = 'Buzz';
+    }
+
+    return output;
+  },
+
+  switcher: function(word) {
+    var output;
+
+    switch (word) {
+      case 'foo':
+        output = 'bar';
+        break;
+      case 'nope':
+        output = 'yup';
+        break;
+      default:
+        output = 'never output anything. ever!'
+    };
+
+    return output;
+  }
+}
